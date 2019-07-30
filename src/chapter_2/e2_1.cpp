8315ec06fd6e75f6d024f9562edf54a31a022b9a
Zepyhrus <sherkfung@gmail.com>
2019-07-30 20:27:23 +0800

    chapter 2

---
+++ src/chapter_2/e2_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+int main(int argc, char** argv) {
+  /* int, long, long, short:
+   *    * size, precision;
+   * unsigned, signed:
+   *    * signed: with no limit, could be negative;
+   *    * unsigned: wrap around, can only be positive;
+   * float, double:
+   *    * size, precision;
+   */
+  return 0;
+}
+
+
---
+++ src/chapter_2/e2_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+  /* rate: double, includes percentage;
+   * principal: double, needs division operations;
+   * payment: double, needs division operations;
+   */
+  return 0;
+}
