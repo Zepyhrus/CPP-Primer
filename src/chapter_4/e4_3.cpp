52a3b3ee9cb731570dd515c24b151e14303e2d78
Zepyhrus <sherkfung@gmail.com>
2019-08-27 21:16:30 +0800

    chapter 4

---
+++ src/chapter_4/e4_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  cout << "I consider this to be a fair-enough trade-off."              << endl;
+  cout << "There is little chance that the order of the evaluations in" << endl;
+  cout << "   one single expression matters."                           << endl;
+  cout << "And even when we draws the benefits from the order of the "  << endl;
+  cout << "   evaluation, it could also easily achieved from seperati-" << endl;
+  cout << "   ng the expression into multiple lines or grouping them. " << endl;
+
+  return 0;
+}
---
+++ src/chapter_4/e4_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 << endl;  // 91
+
+  
+
+  return 0;
+}
